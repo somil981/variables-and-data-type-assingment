047b26538ad3961d518269882f11eaf881c3ccd3
somil981 <140135438+somil981@users.noreply.github.com>
2024-01-11 08:55:37 +0530

    Add files via upload

---
+++ try.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    char character;
+    cin>>character;
+
+    switch (character)
+    {
+    case 'a':
+        cout<<"vovel"<<endl;
+        break;
+
+    case 'e':
+        cout<<"vovel"<<endl;
+        break;
+
+    case 'i':
+        cout<<"vovel"<<endl;
+        break;
+
+    case 'o':
+        cout<<"vovel"<<endl;
+        break;
+
+    case 'u':
+        cout<<"vovel"<<endl;
+        break;              
+
+    default:
+        cout<<"consunant"<<endl;
+        break;                      
+    }
+    
+    return 0;
+
+    }
